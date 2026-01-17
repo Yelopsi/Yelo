b7ebbde6f780e90fe0b2ff44261c5ab31c19f590
sasfandersoncosta-specA <sasf.andersoncosta@gmail.com>
2026-01-17 15:17:59 -0300

    a

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -17,7 +17,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/api/auth/google/callback",
-    passReqToCallback: true // Necessário para ler req.session.registerType
+    passReqToCallback: true, // Necessário para ler req.session.registerType
+    proxy: true // Importante para produção (Render) para garantir HTTPS no callback
 },
 async (req, accessToken, refreshToken, profile, done) => {
     try {
