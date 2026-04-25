68e5300c65b9feedf86e588be103d767d1dfe98e
sasfandersoncosta-specA <sasf.andersoncosta@gmail.com>
2026-04-25 18:53:23 -0300

    ss

---
+++ admin/admin_conteudo_hub.js
@@ -0,0 +1,5 @@
+// c:\Users\Anderson\Desktop\Yelo\admin\admin_conteudo_hub.js
+
+window.initializePage = function() {
+    // Página estática de hub (Painel). Nenhuma inicialização complexa necessária no momento.
+};
